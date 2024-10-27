8f268fe75bff3f7d3f2b1613b7e21597f0c41fd4
kritpi <kritpiruch@gmail.com>
2024-10-27 16:58:06 +0700

    Fix next auth useSession must be wrap with sessionProvider(somehow this error occured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+'use client'
 import type { Metadata } from "next";
 import { Noto_Sans_Thai } from "next/font/google";
 import NextAuthProvider from "@/provider/NextAuthProvider";
+import { SessionProvider } from "next-auth/react";
 import { Providers } from "../provider/NextUiProviders";
 import "./globals.css";
 import { Navbar } from "@/components/components/navbar";
@@ -12,10 +14,10 @@ const notoSanThai = Noto_Sans_Thai({
   subsets: ["latin", "latin-ext", "thai"],
 });
 
-export const metadata: Metadata = {
-  title: "Trip-Booking",
-  description: "Tour Booking Web Application",
-};
+// export const metadata: Metadata = { 
+//   title: "Trip-Booking",
+//   description: "Tour Booking Web Application",
+// };
 
 export default function RootLayout({
   children,
@@ -26,10 +28,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={notoSanThai.className}>
         <NextAuthProvider>
-          <Providers>
-            <Navbar></Navbar>          
-            <div className="grid mx-[15rem] my-[3rem]">{children}</div>
-          </Providers>
+          <SessionProvider>
+            <Providers>
+              <Navbar></Navbar>
+              <div className="grid mx-[15rem] my-[3rem]">{children}</div>
+            </Providers>
+          </SessionProvider>
         </NextAuthProvider>
       </body>
     </html>
